413a6b009ca04e2998f4848a58c555023474979d
kartikanafius <65629440+kartikanafius@users.noreply.github.com>
2020-05-30 11:27:09 +0700

    Adding Individual Task

---
+++ klk/klk/Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace klk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.btn_plusminus = new System.Windows.Forms.Button();
+            this.btn_koma = new System.Windows.Forms.Button();
+            this.btn_tambah = new System.Windows.Forms.Button();
+            this.btn_kurang = new System.Windows.Forms.Button();
+            this.btn_kali = new System.Windows.Forms.Button();
+            this.btn_bagi = new System.Windows.Forms.Button();
+            this.btn_hasil = new System.Windows.Forms.Button();
+            this.btn_hapus = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.hasil = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btn_1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1.Location = new System.Drawing.Point(12, 295);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(68, 64);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = false;
+            this.btn_1.Click += new System.EventHandler(this.btn_click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btn_2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_2.Location = new System.Drawing.Point(79, 295);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(68, 64);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = false;
+            this.btn_2.Click += new System.EventHandler(this.btn_click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btn_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_3.Location = new System.Drawing.Point(146, 295);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(68, 64);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = false;
+            this.btn_3.Click += new System.EventHandler(this.btn_click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btn_4.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_4.Location = new System.Drawing.Point(12, 232);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(68, 64);
+            this.btn_4.TabIndex = 3;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = false;
+            this.btn_4.Click += new System.EventHandler(this.btn_click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btn_5.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_5.Location = new System.Drawing.Point(79, 232);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(68, 64);
+            this.btn_5.TabIndex = 4;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = false;
+            this.btn_5.Click += new System.EventHandler(this.btn_click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btn_6.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_6.Location = new System.Drawing.Point(146, 232);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(68, 64);
+            this.btn_6.TabIndex = 5;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = false;
+            this.btn_6.Click += new System.EventHandler(this.btn_click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btn_7.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_7.Location = new System.Drawing.Point(12, 169);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(68, 64);
+            this.btn_7.TabIndex = 6;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = false;
+            this.btn_7.Click += new System.EventHandler(this.btn_click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btn_8.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_8.Location = new System.Drawing.Point(79, 169);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(68, 64);
+            this.btn_8.TabIndex = 7;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = false;
+            this.btn_8.Click += new System.EventHandler(this.btn_click);
+            // 
+            // btn_9
+            // 
+            this.btn_9.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btn_9.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_9.Location = new System.Drawing.Point(146, 169);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(68, 64);
+            this.btn_9.TabIndex = 8;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = false;
+            this.btn_9.Click += new System.EventHandler(this.btn_click);
+            // 
+            // btn_0
+            // 
+            this.btn_0.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btn_0.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_0.Location = new System.Drawing.Point(79, 358);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(68, 64);
+            this.btn_0.TabIndex = 9;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = false;
+            this.btn_0.Click += new System.EventHandler(this.btn_click);
+            // 
+            // btn_plusminus
+            // 
+            this.btn_plusminus.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_plusminus.Location = new System.Drawing.Point(12, 358);
+            this.btn_plusminus.Name = "btn_plusminus";
+            this.btn_plusminus.Size = new System.Drawing.Size(68, 64);
+            this.btn_plusminus.TabIndex = 10;
+            this.btn_plusminus.Text = "±";
+            this.btn_plusminus.UseVisualStyleBackColor = true;
+            this.btn_plusminus.Click += new System.EventHandler(this.plusminus);
+            // 
+            // btn_koma
+            // 
+            this.btn_koma.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_koma.Location = new System.Drawing.Point(146, 358);
+            this.btn_koma.Name = "btn_koma";
+            this.btn_koma.Size = new System.Drawing.Size(68, 64);
+            this.btn_koma.TabIndex = 11;
+            this.btn_koma.Text = ".";
+            this.btn_koma.UseVisualStyleBackColor = true;
+            this.btn_koma.Click += new System.EventHandler(this.btn_click);
+            // 
+            // btn_tambah
+            // 
+            this.btn_tambah.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_tambah.Location = new System.Drawing.Point(213, 295);
+            this.btn_tambah.Name = "btn_tambah";
+            this.btn_tambah.Size = new System.Drawing.Size(68, 64);
+            this.btn_tambah.TabIndex = 12;
+            this.btn_tambah.Text = "+";
+            this.btn_tambah.UseVisualStyleBackColor = true;
+            this.btn_tambah.Click += new System.EventHandler(this.operator_click);
+            // 
+            // btn_kurang
+            // 
+            this.btn_kurang.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_kurang.Location = new System.Drawing.Point(213, 232);
+            this.btn_kurang.Name = "btn_kurang";
+            this.btn_kurang.Size = new System.Drawing.Size(68, 64);
+            this.btn_kurang.TabIndex = 13;
+            this.btn_kurang.Text = "-";
+            this.btn_kurang.UseVisualStyleBackColor = true;
+            this.btn_kurang.Click += new System.EventHandler(this.operator_click);
+            // 
+            // btn_kali
+            // 
+            this.btn_kali.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_kali.Location = new System.Drawing.Point(213, 169);
+            this.btn_kali.Name = "btn_kali";
+            this.btn_kali.Size = new System.Drawing.Size(68, 64);
+            this.btn_kali.TabIndex = 14;
+            this.btn_kali.Text = "x";
+            this.btn_kali.UseVisualStyleBackColor = true;
+            this.btn_kali.Click += new System.EventHandler(this.operator_click);
+            // 
+            // btn_bagi
+            // 
+            this.btn_bagi.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_bagi.Location = new System.Drawing.Point(213, 106);
+            this.btn_bagi.Name = "btn_bagi";
+            this.btn_bagi.Size = new System.Drawing.Size(68, 64);
+            this.btn_bagi.TabIndex = 15;
+            this.btn_bagi.Text = "÷";
+            this.btn_bagi.UseVisualStyleBackColor = true;
+            this.btn_bagi.Click += new System.EventHandler(this.operator_click);
+            // 
+            // btn_hasil
+            // 
+            this.btn_hasil.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_hasil.Location = new System.Drawing.Point(213, 358);
+            this.btn_hasil.Name = "btn_hasil";
+            this.btn_hasil.Size = new System.Drawing.Size(68, 64);
+            this.btn_hasil.TabIndex = 16;
+            this.btn_hasil.Text = "=";
+            this.btn_hasil.UseVisualStyleBackColor = true;
+            this.btn_hasil.Click += new System.EventHandler(this.hitung);
+            // 
+            // btn_hapus
+            // 
+            this.btn_hapus.Font = new System.Drawing.Font("Wingdings", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btn_hapus.Location = new System.Drawing.Point(111, 106);
+            this.btn_hapus.Name = "btn_hapus";
+            this.btn_hapus.Size = new System.Drawing.Size(103, 64);
+            this.btn_hapus.TabIndex = 17;
+            this.btn_hapus.Text = "";
+            this.btn_hapus.UseVisualStyleBackColor = true;
+            this.btn_hapus.Click += new System.EventHandler(this.bspace);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(12, 106);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(100, 64);
+            this.btn_clear.TabIndex = 18;
+            this.btn_clear.Text = "C";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.bersih);
+            // 
+            // hasil
+            // 
+            this.hasil.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hasil.Location = new System.Drawing.Point(12, 47);
+            this.hasil.Multiline = true;
+            this.hasil.Name = "hasil";
+            this.hasil.Size = new System.Drawing.Size(269, 42);
+            this.hasil.TabIndex = 19;
+            this.hasil.Text = "0";
+            this.hasil.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.hasil.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(296, 24);
+            this.menuStrip1.TabIndex = 20;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.keluar);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(296, 429);
+            this.Controls.Add(this.hasil);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_hapus);
+            this.Controls.Add(this.btn_hasil);
+            this.Controls.Add(this.btn_bagi);
+            this.Controls.Add(this.btn_kali);
+            this.Controls.Add(this.btn_kurang);
+            this.Controls.Add(this.btn_tambah);
+            this.Controls.Add(this.btn_koma);
+            this.Controls.Add(this.btn_plusminus);
+            this.Controls.Add(this.btn_0);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button btn_plusminus;
+        private System.Windows.Forms.Button btn_koma;
+        private System.Windows.Forms.Button btn_tambah;
+        private System.Windows.Forms.Button btn_kurang;
+        private System.Windows.Forms.Button btn_kali;
+        private System.Windows.Forms.Button btn_bagi;
+        private System.Windows.Forms.Button btn_hasil;
+        private System.Windows.Forms.Button btn_hapus;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.TextBox hasil;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ klk/klk/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace klk
+{
+    public partial class Form1 : Form
+    {
+        double value = 0;
+        string ops = "";
+        bool op_pressed = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_click(object sender, EventArgs e)
+        {
+            if ((hasil.Text == "0") || (op_pressed))
+            {
+                hasil.Clear();
+            }
+            Button b = (Button)sender;
+            hasil.Text = hasil.Text + b.Text;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            ops = b.Text;
+            value = Double.Parse(hasil.Text);
+
+            op_pressed = true;
+        }
+
+        private void hitung(object sender, EventArgs e)
+        {
+            switch (ops)
+            {
+                case "+":
+                    hasil.Text = (value + Double.Parse(hasil.Text)).ToString();
+                    break;
+                case "-":
+                    hasil.Text = (value - Double.Parse(hasil.Text)).ToString();
+                    break;
+                case "x":
+                    hasil.Text = (value * Double.Parse(hasil.Text)).ToString();
+                    break;
+                case "÷":
+                    hasil.Text = (value / Double.Parse(hasil.Text)).ToString();
+                    break;
+            }
+        }
+
+        private void bersih(object sender, EventArgs e)
+        {
+            hasil.Text = "0";
+        }
+
+        private void bspace(object sender, EventArgs e)
+        {
+            hasil.Text = hasil.Text.Remove(hasil.Text.Length - 1, 1);
+        }
+
+        private void keluar(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void plusminus(object sender, EventArgs e)
+        {
+            if (hasil.Text.Contains("-"))
+
+                hasil.Text = hasil.Text.Remove(0, 1);
+            else
+
+                hasil.Text = "-" + hasil.Text;
+        }
+    }
+}
